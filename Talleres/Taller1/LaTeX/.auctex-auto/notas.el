dcb537ef7c265c8ce9b707d22cc75617a28f7baf
TheLudway <theludway@gmail.com>
2025-02-26 14:12:30 -0500

    Puntos del taller

---
+++ Talleres/Taller1/LaTeX/.auctex-auto/notas.el
@@ -0,0 +1,27 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "notas"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("graphicx" "") ("amsmath" "") ("geometry" "margin=1in") ("fancyhdr" "") ("enumerate" "") ("enumitem" "shortlabels") ("babel" "spanish") ("xurl" "") ("tcolorbox" "") ("titlesec" "")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "graphicx"
+    "amsmath"
+    "geometry"
+    "fancyhdr"
+    "enumerate"
+    "enumitem"
+    "babel"
+    "xurl"
+    "tcolorbox"
+    "titlesec")
+   (LaTeX-add-counters
+    "subsubsubsection"))
+ :latex)
+

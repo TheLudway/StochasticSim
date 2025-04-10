44bf2c7670c3c9d052eea2edfb6d0d94be55d39e
TheLudway <theludway@gmail.com>
2025-04-09 21:35:24 -0500

    Punto 4 y 5 completados

--- Talleres/Taller2/LaTeX/run.R
+++ Talleres/Taller2/LaTeX/run.R
@@ -1,5 +1,13 @@
 #!/usr/bin/Rscript
 
-knitr::Sweave2knitr("notas.Rnw")
+# Elimina esta línea innecesaria
+# knitr::clean_cache()
+
+# Si estás usando sintaxis Sweave (<<>>= @), no necesitas Sweave2knitr
+# knitr::Sweave2knitr("notas.Rnw")  # solo si vas a convertir a R Markdown
+
+# Procesa el documento con knitr
 knitr::knit("notas.Rnw")
+
+# Compila a PDF
 tools::texi2pdf("notas.tex")
